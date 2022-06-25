294e34cabe2faa41d5c12a37b2499fac0f15b745
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-06-25 22:39:01 +0200

    Compleated white part of the navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
 import NavigationBlack from './components/navigationBlack/navigationBlack';
+import NavigationWhite from './components/navigationWhite/navigationWhite';
 
 function App() {
   return (
     <div className="App">
         <NavigationBlack/>
+        <NavigationWhite/>
     </div>
   );
 }
