130d64b52a0bbe98d4aa73d894b6b44d069f9b58
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-06-27 17:15:39 +0200

    Fixed PR issues

--- Backend/src/main/java/com/praksa/auction/AuctionApplication.java
+++ Backend/src/main/java/com/praksa/auction/AuctionApplication.java
@@ -1,11 +1,14 @@
 package com.praksa.auction;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
 public class AuctionApplication {
 
 	public static void main(String[] args) {
