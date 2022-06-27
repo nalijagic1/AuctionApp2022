4933a13c9d982e7e05f6992411d693732d92ef4c
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-06-27 20:45:34 +0200

    Added security configuration

---
+++ Backend/src/main/java/com/praksa/auction/WebSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.praksa.auction;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfiguration {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+            http.authorizeHttpRequests().anyRequest().permitAll();
+            return  http.build();
+}
+}
