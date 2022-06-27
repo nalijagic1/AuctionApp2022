2251e9083a48e8cf9391d0ef5d1e839250dddaf0
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-06-28 00:13:55 +0200

    Removing unused imports

--- Backend/src/main/java/com/praksa/auction/AuctionApplication.java
+++ Backend/src/main/java/com/praksa/auction/AuctionApplication.java
@@ -1,10 +1,9 @@
 package com.praksa.auction;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
