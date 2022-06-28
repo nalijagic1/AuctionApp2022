6dfa6ff40b5985f1ceaf0a27899c961d5323d403
nalijagic1 <62034906+nalijagic1@users.noreply.github.com>
2022-06-28 09:14:40 +0200

    Project skeleton (#1)
    * Swagger succesfully ran
    
    * Names changed to camelCase
    
    * changed frontend file name to lowercase
    
    * Delete frontend directory
    
    * Frontend package.json retrieval
    
    * Fixed PR issues
    
    * Added security configuration
    
    * Removing unused imports

--- Backend/src/main/java/com/praksa/auction/AuctionApplication.java
+++ Backend/src/main/java/com/praksa/auction/AuctionApplication.java
@@ -2,8 +2,12 @@ package com.praksa.auction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
 public class AuctionApplication {
 
 	public static void main(String[] args) {
