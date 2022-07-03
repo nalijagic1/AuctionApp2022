46b00ffa263108a96a8d177a42063b8dfc3953b5
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-07-04 00:16:45 +0200

    Implemented category list section

---
+++ frontend/src/components/categoryList/categoryList,.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./categoryList.css"
+import categoryService from '../../services/category.service';
+import { useEffect, useState } from "react";
+function CategoryList() {
+  const [categories,setCategories] = useState()
+  useEffect(() => {
+    categoryService.getAll()
+        .then((response) => {
+          setCategories(response.data)
+      });
+  }, []);
+  return(
+    <div className="list">
+      <p>CATEGORIES</p>
+      <ul className='category'>
+        {categories && categories.map(cat => (
+          <li  key={cat.id}>{cat.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CategoryList;
