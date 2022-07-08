f0f913ac30894072535c3da6480dc0eeca342901
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-07-08 21:08:38 +0200

    Added timer

--- frontend/src/components/productInfo/productInfo.js
+++ frontend/src/components/productInfo/productInfo.js
@@ -4,9 +4,12 @@ import Button from '../button/button';
 import arrow from '../../images/arrow.png'
 import './productInfo.css'
 import bidService from '../../services/bid.service';
+import moment from 'moment';
 function ProductInfo({product}) {
     const[highestBid,setHighestBid] = useState([]);
     const[count,setCount] = useState(0);
+    const diferece = moment().diff(moment(product.endingDate,'YYYY-MM-DD'))
+    var duration = moment.duration(diferece)
     useEffect(() => {
         bidService.getHighestBid(product.id).then(
             (response)=>{
@@ -28,12 +31,16 @@ function ProductInfo({product}) {
       <div className='biddingInfo'> 
            <h3>Highest bid: <a>${highestBid.bid}</a></h3> 
            <h3>Number of bids: <a>{count}</a></h3>
-           <h3>Time left:</h3>
+           <h3>Time left: <a>{duration.humanize()}</a></h3>
       </div>
       <div className='bid'>
         <Field placeHolder={`Enter $${highestBid.bid+1} or higher`}/>
         <Button lable ="Place bid" icon={arrow}/>
       </div>
+      <div className="desc">
+        <h3>Details</h3>
+      </div>
+      <p>{product.description}</p>
      </div>
   );
 }
