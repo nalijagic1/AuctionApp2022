aedaf91edc954440871c1c0eed4edbc0f82d687f
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-07-20 16:45:17 +0200

    Change in querry

--- backend/src/main/java/com/praksa/auction/repository/CategoryRepository.java
+++ backend/src/main/java/com/praksa/auction/repository/CategoryRepository.java
@@ -4,12 +4,11 @@ import com.praksa.auction.dto.SubcategoryDto;
 import com.praksa.auction.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query(value = "SELECT NEW com.praksa.auction.dto.SubcategoryDto(s.id, s.name, COUNT(p.subcategory)) FROM Subcategory s LEFT JOIN Product p ON(s = p.subcategory) WHERE s.category.id = :category GROUP BY s.name, s.id", nativeQuery = false)
-    List<SubcategoryDto> findSubcategoriesWithCount(@Param("category") long category);
+    @Query("SELECT NEW com.praksa.auction.dto.SubcategoryDto(s.id, s.name, COUNT(p.subcategory)) FROM Subcategory s LEFT JOIN Product p ON(s = p.subcategory) WHERE s.category.id = :category GROUP BY s.name, s.id")
+    List<SubcategoryDto> findSubcategoriesWithCount(long category);
 }
