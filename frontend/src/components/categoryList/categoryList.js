da64c060c66e244fdf00e50cefddc84571960f15
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-07-20 13:52:58 +0200

    Showing subcategories fixed

--- frontend/src/components/categoryList/categoryList.js
+++ frontend/src/components/categoryList/categoryList.js
@@ -3,86 +3,41 @@ import "./categoryList.css"
 import categoryService from '../../services/category.service';
 import {useEffect, useState} from "react";
 import {Link} from 'react-router-dom';
-import plus from '../../images/Plus.png'
-import minus from "../../images/Minus.png"
+import { useNavigate } from 'react-router';
+import { useLocation } from 'react-router-dom'
+import {TiPlus, TiMinus} from 'react-icons/ti';
 
 function CategoryList({filter}) {
     const [categories, setCategories] = useState();
-    const [refresh,setRefresh] = useState(0);
-    let show = useRef([]);
-    let expand = useRef([]);
-    let mainContainerClass = "list";
-    let listContainerClass = "listItem";
-    let previousCategory = useRef(-1);
-    const [sign, setSign] = useState(-1);
+    const navigate = useNavigate();
+    const location = useLocation();
     let prodCat = useRef("");
 
-    function showSubcategories(index) {
-        let showArray = [...show.current];
-        let expandArray = [...expand.current];
-       if (expand.current[index] === plus) {
-            expandArray[index]= minus;
-            showArray[index] = true;
-        } else {
-            expandArray[index] = plus;
-            showArray[index] = false;
-        }
-        show.current = showArray;
-        expand.current = expandArray;
-        setRefresh(r => r + 1);
+    function showSubcategories(selectedCategory) {
+        if(location.pathname.includes(selectedCategory)) navigate("/shop/all");
+        else navigate("/shop/" + selectedCategory);
     }
 
     useEffect(() => {
         categoryService.getCategoriesWithSubcategories()
             .then((response) => {
                 setCategories(response.data);
-                if (filter) {
-                    prodCat.current = "PRODUCT ";
-                    let selected = response.data.find(element => element.category.name.toLowerCase() === filter.toLowerCase());
-                    if (previousCategory.current !== -2 && (!selected  || previousCategory.current !== selected.category.id - 1)) {
-                        show.current = new Array(response.data.length).fill(false);
-                        expand.current = new Array(response.data.length).fill(plus);
-                    }
-                    if (selected) {
-                        show.current[selected.category.id - 1] = true;
-                        if (previousCategory.current < 0 ) {
-                            expand.current[selected.category.id - 1] = minus;
-                        } else setSign(1);
-                        previousCategory.current = selected.category.id - 1;
-
-                    } else {
-                        if(previousCategory.current === -1) {
-                            previousCategory.current = -2;
-                            setSign(-2);
-                        }
-                    }
-                }
             })
-        if (sign === 1) {
-            expand.current[previousCategory.current] = minus;
-        }
+    }, []);
 
-    }, [filter, sign, refresh]);
-    if (filter) {
-        mainContainerClass += " filter";
-        listContainerClass += "filter";
-    }
     return (
-        <div className={mainContainerClass}>
+        <div className={filter ? "list filter" : "list"}>
             <p>{prodCat.current}CATEGORIES</p>
             <ul className='category'>
                 {categories && categories.map(cat => (
                     <div key={cat.category.id}>
-                        <div className={listContainerClass}>
-                            <Link to={`/shop/${cat.category.name}`}>
+                        <div className={filter ? "listItemfilter" : "listItem"} onClick={() => showSubcategories(cat.category.name)}>
                                 <li>{cat.category.name} </li>
-                            </Link>
-                            {filter &&
-                            <img id={cat.category.id} className="showMore" src={expand.current[cat.category.id-1]} onClick={event => showSubcategories(event.target.id-1)}
-                                 alt="sign"></img>
-                            }
+                                {filter && 
+                                    <div>{filter === cat.category.name ? <TiMinus className='icon' style={{fontSize: 16, color: '#252525'}}/> : <TiPlus  className='icon' style={{fontSize: 16, color: '#252525'}}/>}</div>
+                                }
                         </div>
-                        {filter && show.current[cat.category.id - 1] && cat.subcategories.map(sub => (
+                        {filter && filter === cat.category.name &&  cat.subcategories.map(sub => (
                             <ul  key = {sub.id} className='listSubcategory'>
                                 <li>{sub.name} ({sub.count})</li>
                             </ul>
