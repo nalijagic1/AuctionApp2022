0ec2fca3ad1a49cefe1e543a2c8dbee34c805605
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-08-22 13:30:20 +0200

    Notification fade in and fade out effect

--- backend/src/main/java/com/praksa/auction/repository/CategoryRepository.java
+++ backend/src/main/java/com/praksa/auction/repository/CategoryRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query("SELECT NEW com.praksa.auction.dto.SubcategoryDto(s.id, s.name, COUNT(p.subcategory)) FROM Subcategory s LEFT JOIN Product p ON(s = p.subcategory) WHERE s.category.id = :category GROUP BY s.name, s.id")
+    @Query("SELECT NEW com.praksa.auction.dto.SubcategoryDto(s.id, s.name, COUNT(p.subcategory)) FROM Subcategory s LEFT JOIN Product p ON(s = p.subcategory) WHERE s.category.id = :category AND p.endingDate > CURRENT_DATE GROUP BY s.name, s.id")
     List<SubcategoryDto> findSubcategoriesWithCount(long category);
 }
