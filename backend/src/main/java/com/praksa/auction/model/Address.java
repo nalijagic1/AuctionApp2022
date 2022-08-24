36134219630a9367c1fe9dba7da51328d0d6300c
nalijagic1 <nalijagic1@etf.unsa.ba>
2022-08-24 23:48:08 +0200

    Adding lombok to class models

--- backend/src/main/java/com/praksa/auction/model/Address.java
+++ backend/src/main/java/com/praksa/auction/model/Address.java
@@ -1,8 +1,16 @@
 package com.praksa.auction.model;
 
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "address")
 public class Address {
@@ -16,67 +24,4 @@ public class Address {
     @ManyToOne
     @JoinColumn(name = "countryId")
     private Country country;
-
-
-    public Address(long id, String street, String city, String zipCode, String state, Country country) {
-        this.id = id;
-        this.street = street;
-        this.city = city;
-        this.zipCode = zipCode;
-        this.state = state;
-        this.country = country;
-    }
-
-    public Address() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public Country getCountry() {
-        return country;
-    }
-
-    public void setCountry(Country country) {
-        this.country = country;
-    }
-
-
 }
--- backend/src/main/java/com/praksa/auction/model/Bid.java
+++ backend/src/main/java/com/praksa/auction/model/Bid.java
@@ -1,9 +1,18 @@
 package com.praksa.auction.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "bid")
 public class Bid {
@@ -18,56 +27,4 @@ public class Bid {
     private Person person;
     private Double bid;
     private Date bidDate;
-
-    public Bid(Product product, Person person, Double bid, Date bidDate) {
-        this.product = product;
-        this.person = person;
-        this.bid = bid;
-        this.bidDate = bidDate;
-    }
-
-    public Bid() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
-    }
-
-    public Double getBid() {
-        return bid;
-    }
-
-    public void setBid(Double bid) {
-        this.bid = bid;
-    }
-
-    public Date getBidDate() {
-        return bidDate;
-    }
-
-    public void setBidDate(Date bidDate) {
-        this.bidDate = bidDate;
-    }
-
-
 }
